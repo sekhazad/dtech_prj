446affe7c5cb2175d8f61624bdd239fbb46c72ea
Abulkalamazad Sekh <sekhazad740@gmail.com>
2019-06-07 21:43:54 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+//------------------Gallery----------------------
+$(function(){
+    $(".more-img").hide()
+    $(".mybtn").click(function(){
+      $(".more-img").fadeToggle()
+    });
+});
+///-------------------Form validation------------------------
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
